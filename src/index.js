20f675a9ed39388754ef578406c4e910b62cd2f2
EimiMatsuzaki <gt.amy813@gmail.com>
2024-02-07 13:01:24 +0900

    ハンバーガーメニューPC版・Flowno

--- src/index.js
+++ src/index.js
@@ -106,3 +106,18 @@ $(document).ready(function () {
 		$indicators.eq(nextSlide).addClass("active");
 	});
 });
+
+/*----------------------------
+    Accordion
+    アコーディオン
+----------------------------*/
+$('.sp_accordion').on('click', function() {//タイトル要素をクリックしたら
+	var findElm = $(this).next(".sp_accordion_content");//直後のアコーディオンを行うエリアを取得し
+	$(findElm).slideToggle();//アコーディオンの上下動作
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去し
+	}else{//それ以外は
+		$(this).addClass('close');//クラス名closeを付与
+	}
+});
