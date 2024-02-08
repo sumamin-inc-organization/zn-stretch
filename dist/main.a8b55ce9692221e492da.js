ff0f537852070af9b4e3a3dac1c0da49ceade159
EimiMatsuzaki <gt.amy813@gmail.com>
2024-02-08 20:04:30 +0900

    2月8日最終版

--- dist/main.a8b55ce9692221e492da.js
+++ dist/main.a8b55ce9692221e492da.js
@@ -1,24 +1,308 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/css/about.css":
+/*!**********************************!*\
+  !*** ./src/assets/css/about.css ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/area.css":
+/*!*********************************!*\
+  !*** ./src/assets/css/area.css ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/common/animation.css":
+/*!*********************************************!*\
+  !*** ./src/assets/css/common/animation.css ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/common/reset.css":
+/*!*****************************************!*\
+  !*** ./src/assets/css/common/reset.css ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/common/typography.css":
+/*!**********************************************!*\
+  !*** ./src/assets/css/common/typography.css ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/common/utility.css":
+/*!*******************************************!*\
+  !*** ./src/assets/css/common/utility.css ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/entry.css":
+/*!**********************************!*\
+  !*** ./src/assets/css/entry.css ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/flow.css":
+/*!*********************************!*\
+  !*** ./src/assets/css/flow.css ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/footer.css":
+/*!***********************************!*\
+  !*** ./src/assets/css/footer.css ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/header.css":
+/*!***********************************!*\
+  !*** ./src/assets/css/header.css ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/interview.css":
+/*!**************************************!*\
+  !*** ./src/assets/css/interview.css ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/keyvisual.css":
+/*!**************************************!*\
+  !*** ./src/assets/css/keyvisual.css ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/navigation.css":
+/*!***************************************!*\
+  !*** ./src/assets/css/navigation.css ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/point.css":
+/*!**********************************!*\
+  !*** ./src/assets/css/point.css ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/precedent.css":
+/*!**************************************!*\
+  !*** ./src/assets/css/precedent.css ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/recruit.css":
+/*!************************************!*\
+  !*** ./src/assets/css/recruit.css ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/schedule.css":
+/*!*************************************!*\
+  !*** ./src/assets/css/schedule.css ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/css/slick.css":
+/*!**********************************!*\
+  !*** ./src/assets/css/slick.css ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _assets_css_common_animation_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./assets/css/common/animation.css */ "./src/assets/css/common/animation.css");
+/* harmony import */ var _assets_css_common_reset_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./assets/css/common/reset.css */ "./src/assets/css/common/reset.css");
+/* harmony import */ var _assets_css_common_typography_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./assets/css/common/typography.css */ "./src/assets/css/common/typography.css");
+/* harmony import */ var _assets_css_common_utility_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./assets/css/common/utility.css */ "./src/assets/css/common/utility.css");
+/* harmony import */ var _assets_css_about_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./assets/css/about.css */ "./src/assets/css/about.css");
+/* harmony import */ var _assets_css_area_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./assets/css/area.css */ "./src/assets/css/area.css");
+/* harmony import */ var _assets_css_entry_css__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./assets/css/entry.css */ "./src/assets/css/entry.css");
+/* harmony import */ var _assets_css_flow_css__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./assets/css/flow.css */ "./src/assets/css/flow.css");
+/* harmony import */ var _assets_css_footer_css__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./assets/css/footer.css */ "./src/assets/css/footer.css");
+/* harmony import */ var _assets_css_header_css__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./assets/css/header.css */ "./src/assets/css/header.css");
+/* harmony import */ var _assets_css_interview_css__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./assets/css/interview.css */ "./src/assets/css/interview.css");
+/* harmony import */ var _assets_css_keyvisual_css__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./assets/css/keyvisual.css */ "./src/assets/css/keyvisual.css");
+/* harmony import */ var _assets_css_navigation_css__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./assets/css/navigation.css */ "./src/assets/css/navigation.css");
+/* harmony import */ var _assets_css_point_css__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./assets/css/point.css */ "./src/assets/css/point.css");
+/* harmony import */ var _assets_css_precedent_css__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./assets/css/precedent.css */ "./src/assets/css/precedent.css");
+/* harmony import */ var _assets_css_recruit_css__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! ./assets/css/recruit.css */ "./src/assets/css/recruit.css");
+/* harmony import */ var _assets_css_schedule_css__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! ./assets/css/schedule.css */ "./src/assets/css/schedule.css");
+/* harmony import */ var _assets_css_slick_css__WEBPACK_IMPORTED_MODULE_17__ = __webpack_require__(/*! ./assets/css/slick.css */ "./src/assets/css/slick.css");
 /*----------------------------
     CSS / スタイリングシート
 ----------------------------*/
-import "./assets/css/common/animation.css";
-import "./assets/css/common/reset.css";
-import "./assets/css/common/typography.css";
-import "./assets/css/common/utility.css";
-import "./assets/css/about.css";
-import "./assets/css/area.css";
-import "./assets/css/entry.css";
-import "./assets/css/flow.css";
-import "./assets/css/footer.css";
-import "./assets/css/header.css";
-import "./assets/css/interview.css";
-import "./assets/css/keyvisual.css";
-import "./assets/css/navigation.css";
-import "./assets/css/point.css";
-import "./assets/css/precedent.css";
-import "./assets/css/recruit.css";
-import "./assets/css/schedule.css";
-import "./assets/css/slick.css";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*----------------------------
     Color change / スクロール位置に応じて色変更
@@ -453,17 +737,6 @@ function fadeAnime(){
 		}
 	  });
 	
-	//Gradient
-	$('.bg_gradient_Trigger').each(function(){ //bgUDextendTriggerというクラス名が
-		var elemPos = $(this).offset().top+200;//要素より、50px上の
-		var scroll = $(window).scrollTop();
-		var windowHeight = $(window).height();
-		if (scroll >= elemPos - windowHeight){
-		  $(this).addClass('bg_gradient');// 画面内に入ったらbgUDextendというクラス名を追記
-		}else{
-		  $(this).removeClass('bg_gradient');// 画面外に出たらbgUDextendというクラス名を外す
-		}
-	  });
   
 	
   }
@@ -478,3 +751,8 @@ function fadeAnime(){
 	  fadeAnime();/* アニメーション用の関数を呼ぶ*/
 	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
   
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.a8b55ce9692221e492da.js.map
